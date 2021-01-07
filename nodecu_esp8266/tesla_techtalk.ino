d687ae0b815f3095ca558deef681012405501603
Gita Alekhya Paul <gitaalekhyapaul@gmail.com>
2021-01-07 16:57:51 +0530

    chore(iot): added iot assets

--- nodecu_esp8266/tesla_techtalk.ino
+++ nodecu_esp8266/tesla_techtalk.ino
@@ -1,47 +1,64 @@
+/*Impoting Libraries*/
 #include <ESP8266WiFi.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 #include <ArduinoJson.h>
 
+/*Declaring Pin Numbers for the LEDs*/
 const int redPin = D6;
 const int yellowPin = D5;
 const int greenPin = D1;
 
+/*Initialising the WiFiManager Module*/
 WiFiManager wifiManager;
+
+/*Starting a Web Server at Port 8080*/
 ESP8266WebServer server(8080);
 
+/*Registering event handlers*/
 void handleGetUptime();
 void handlePostChangeLed();
-void handleGetSensor();
+void handleGetSensorValue();
 void handle404NotFound();
 
+/*Setup only runs once*/
 void setup()
 {
+  /*Starting the serial connection and checking for past WiFi connections to connect to*/
   Serial.begin(115200);
   wifiManager.autoConnect();
   Serial.printf("LOG:: Connected to WiFi\nLOG:: IP ");
   Serial.println(WiFi.localIP());
+
+  /*Specifying the Digital and Analog Pins*/
   pinMode(redPin, OUTPUT);
   pinMode(yellowPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(A0, INPUT);
+
+  /*Server Route Handlers*/
   server.on("/api/v1/getUptime", HTTP_GET, handleGetUptime);
   server.on("/api/v1/changeLed", HTTP_POST, handlePostChangeLed);
-  server.on("/api/v1/sensor", HTTP_GET, handleGetSensor);
+  server.on("/api/v1/getSensorValue", HTTP_GET, handleGetSensorValue);
   server.onNotFound(handle404NotFound);
+
+  /*Starting the server*/
   server.begin();
   Serial.println("LOG:: Listening for requests on Port 8080");
 }
 
+/*Loop goes on executing for the lifetime*/
 void loop()
 {
   server.handleClient();
 }
 
+/*GET - /api/v1/getUptime*/
 void handleGetUptime()
 {
   Serial.println("LOG:: GET /api/v1/getUptime");
+  /*We use ArduinoJson Library to create and parse JSON objects*/
   DynamicJsonDocument res(1024);
   res["success"] = true;
   res["uptime"] = millis();
@@ -50,6 +67,7 @@ void handleGetUptime()
   server.send(200, "application/json", response);
 }
 
+/*POST - /api/v1/changeLed*/
 void handlePostChangeLed()
 {
   Serial.println("LOG:: POST /api/v1/changeLed");
@@ -92,12 +110,14 @@ void handlePostChangeLed()
   switch (s)
   {
   case 1:
+    /*Switching an LED on by pulling it high*/
     digitalWrite(ledPin, HIGH);
     Serial.println("LOG:: LED is switched ON");
     res["success"] = true;
     res["ledStatus"] = "ON";
     break;
   case 0:
+    /*Switching an LED off by pulling it low*/
     digitalWrite(ledPin, LOW);
     Serial.println("LOG:: LED is switched OFF");
     res["success"] = true;
@@ -123,6 +143,27 @@ void handlePostChangeLed()
   }
 }
 
+/*GET - /api/v1/getSensorValue*/
+void handleGetSensorValue()
+{
+  /*
+  NodeMCU has a 10-bit DAC. Thus the analogRead values range from 0 -1023
+  for 0 - 3.3 volts. On top of that we use map() to convert the above value to a
+  scale of 0 -100
+  */
+  int sensorValue = map(analogRead(A0), 0, 1024, 0, 100);
+  Serial.println("LOG:: GET /api/v1/getSensorValue");
+  Serial.print("Sensor Value: ");
+  Serial.println(sensorValue);
+  DynamicJsonDocument res(1024);
+  res["success"] = true;
+  res["sensorValue"] = sensorValue;
+  char response[1024];
+  serializeJson(res, response);
+  server.send(200, "application/json", response);
+}
+
+/*404 Route Handler*/
 void handle404NotFound()
 {
   Serial.println("API_ERROR:: 404 RESOURCE NOT FOUND");
@@ -132,18 +173,4 @@ void handle404NotFound()
   char response[1024];
   serializeJson(res, response);
   server.send(404, "application/json", response);
-}
-
-void handleGetSensor()
-{
-  int sensorValue = analogRead(A0);
-  Serial.println("LOG:: GET /sensor");
-  Serial.print("Sensor Value: ");
-  Serial.println(sensorValue);
-  DynamicJsonDocument res(1024);
-  res["success"] = true;
-  res["sensorValue"] = sensorValue;
-  char response[1024];
-  serializeJson(res, response);
-  server.send(200, "application/json", response);
 }
