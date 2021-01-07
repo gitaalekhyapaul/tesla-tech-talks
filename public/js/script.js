a85e8cd149cdb4be00481ca508f6b0220f44abf5
Gita Alekhya Paul <gitaalekhyapaul@gmail.com>
2021-01-07 21:31:33 +0530

    feat(client): add client Co-authored-by: Snehil <snehilwebber@gmail.com>

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,85 @@
-alert("Hello World!");
+const authScreen = document.getElementById("auth");
+const mainScreen = document.getElementById("main");
+
+const loginInput = document.getElementById("loginInput");
+const loginButton = document.getElementById("loginButton");
+
+const startButton = document.getElementById("startTraffic");
+const stopButton = document.getElementById("stopTraffic");
+
+const aqiField = document.getElementById("aqi");
+
+const red = document.getElementById("red");
+const yellow = document.getElementById("yellow");
+const green = document.getElementById("green");
+
+const render = () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    mainScreen.style.display = "grid";
+    authScreen.style.display = "none";
+
+    startButton.addEventListener("click", () => {
+      yellow.classList.add("on");
+      red.classList.remove("on");
+
+      fetch("/api/v1/traffic/start", {
+        method: "POST",
+        headers: { token: token },
+      }).then(() => {
+        yellow.classList.remove("on");
+        green.classList.add("on");
+      });
+    });
+
+    stopButton.addEventListener("click", () => {
+      yellow.classList.add("on");
+      green.classList.remove("on");
+
+      fetch("/api/v1/traffic/stop", {
+        method: "POST",
+        headers: { token: token },
+      }).then(() => {
+        yellow.classList.remove("on");
+        red.classList.add("on");
+      });
+    });
+
+    setInterval(() => {
+      fetch("/api/v1/getAqiSensorValue", { headers: { token: token } })
+        .then((response) => response.json())
+        .then((data) => {
+          aqiField.innerText = data.aqiSensor;
+        });
+    }, 2000);
+  } else {
+    mainScreen.style.display = "none";
+    authScreen.style.display = "grid";
+  }
+};
+
+const handleLogin = () => {
+  const password = loginInput.value;
+  console.log(password);
+  if (password.length > 0) {
+    fetch("/api/v1/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ password: password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const { authToken } = data;
+        localStorage.setItem("token", authToken);
+        render();
+      })
+      .catch(() => {
+        alert("API error");
+      });
+  }
+};
+
+loginButton.addEventListener("click", handleLogin);
+render();
